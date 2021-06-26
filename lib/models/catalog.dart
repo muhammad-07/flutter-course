291afc134b850b32e4e3c636e676cea639ff37c7
Muhammad-07 <muhammad.begawala@gmail.com>
2021-06-27 00:02:17 +0530

    before making code short

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,14 +1,28 @@
 class CatalogModel {
-  static final items = [
-  Item(
-      id: 1,
-      name: "AAA",
-      desc: "DESC",
-      color: "#33505a",
-      price: 20.0,
-      image:
-          "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdjc5MS10YW5nLTM1LnBuZw.png?s=aLxshBxLcykO2UAnr6F0Nzhqtdx6iR6UuKi4bFSTzC8")
-];
+  // static final items = [
+  //   Item(
+  //       id: 1,
+  //       name: "AAA",
+  //       desc: "DESC",
+  //       color: "#33505a",
+  //       price: 20.0,
+  //       image:
+  //           "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdjc5MS10YW5nLTM1LnBuZw.png?s=aLxshBxLcykO2UAnr6F0Nzhqtdx6iR6UuKi4bFSTzC8")
+  // ];
+
+
+  //DEFAULT VALUES
+  // static List<Item> items = [
+  //   Item(
+  //       id: 1,
+  //       name: "Default",
+  //       desc: "DESC",
+  //       color: "#33505a",
+  //       price: 0.0,
+  //       image:
+  //           "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdjc5MS10YW5nLTM1LnBuZw.png?s=aLxshBxLcykO2UAnr6F0Nzhqtdx6iR6UuKi4bFSTzC8")
+  // ];
+  static List<Item>? items;
 }
 
 class Item {
@@ -19,6 +33,7 @@ class Item {
   final String image;
   final num price;
 
+// This constructor is For fetvhing hardcoded data
   Item(
       {required this.id,
       required this.name,
@@ -26,6 +41,33 @@ class Item {
       required this.color,
       required this.image,
       required this.price});
+
+// This constructor is For fetching Map json data from json file
+
+// to map data we are getting into our class
+// factory is used when we have more than one constrs
+// Strings means key type and dynamic means value type, that we are receiving from source
+
+//  Commenting to test data dart class generator plugin that auto generats
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      color: map["color"],
+      image: map["image"],
+      price: map["price"],
+    );
+  }
+
+  // to convert class into map MAY  REQUIRE in FUTURE to store data
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "color": color,
+        "image": image,
+        "price": price,
+      };
+  
 }
-
-
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,30 +1,62 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:magfee/widgets/drawer.dart';
 import 'package:magfee/models/catalog.dart';
 import 'package:magfee/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    // Its used to fetch data before BUILD is called so we can pass tO BILD
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    await Future.delayed(Duration(seconds: 3));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["items"];
+    // print(productsData);
+    //4:32
+    // List<Item> list =
+    CatalogModel.items =
+        List.from(productsData).map<Item>((e) => Item.fromMap(e)).toList();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(title: Text("Header")),
         body:
             // Center(child: Container(child: Text("Magfee")),
             Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView.builder(
-            itemCount: dummyList.length,
-            // itemCount: CatalogModel.items.length,
+          child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty) ? ListView.builder(
+            // itemCount: dummyList.length,
+            itemCount: CatalogModel.items!.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                // item: CatalogModel.items[index],
-                item: dummyList[index],
+                item: CatalogModel.items![index],
+                // item: dummyList[index],
               );
             },
-          ),
+          ) : Center(child: CircularProgressIndicator(),),
         ),
         drawer: MyDrawer());
   }
