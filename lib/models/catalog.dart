0d43f3a7913f8999a68687547b7017f281a606dd
Muhammad-07 <muhammad.begawala@gmail.com>
2021-06-26 13:05:21 +0530

    doneDay10

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String color;
+  final String image;
+  final num price;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.color,
+      required this.image,
+      required this.price});
+}
+
+final products = [
+  Item(
+      id: "001",
+      name: "AAA",
+      desc: "DESC",
+      color: "#33505a",
+      price: 20.0,
+      image:
+          "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdjc5MS10YW5nLTM1LnBuZw.png?s=aLxshBxLcykO2UAnr6F0Nzhqtdx6iR6UuKi4bFSTzC8")
+];
